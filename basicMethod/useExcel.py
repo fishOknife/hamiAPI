5bb0209fd3ab4d8c950ce22d7a3d68941511915a
banjiemei <405091274@qq.com>
2021-08-30 11:18:56 +0800

    修改为ddt数据驱动

--- basicMethod/useExcel.py
+++ basicMethod/useExcel.py
@@ -1,23 +1,32 @@
+import sys
 import pandas as pd
 
 
+# 将数据保存至列表中（数据驱动ddt）
+def get_excel_data(file_path, sheet_name):
+    df = pd.read_excel(file_path, sheet_name, keep_default_na=False, usecols=[3, 4, 5])
+    list_data = df.values.tolist()
+    return list_data
+
+
 # 将excel中的数据组合为字典形式，保存在列表中
-def getExcelData(filePath, sheetName):
-    df = pd.read_excel(filePath, sheet_name=sheetName, keep_default_na=False)
-    test_data = []
-    # 获取表头
-    keys = df.columns.values
-    # 获取行号索引，并遍历
-    for i in df.index.values:
-        row_data = df.loc[i, keys].to_dict()
-        test_data.append(row_data)
-    return test_data
+# def getExcelData(filePath, sheetName):
+#     df = pd.read_excel(filePath, sheet_name=sheetName, keep_default_na=False, usecols=[3, 4, 5])
+#     test_data = []
+#     # 获取表头
+#     keys = df.columns.values
+#     # 获取行号索引，并遍历
+#     for i in df.index.values:
+#         row_data = df.loc[i, keys].to_dict()
+#         test_data.append(row_data)
+#     return test_data
 
 
 if __name__ == "__main__":
-    filePath = r"F:\Python38\APITestModel\testData\testData.xlsx"
+    rootDir = sys.path[1]
+    filePath = rootDir+r"\testData\testData.xlsx"
     sheetName = "登录"
-    testData = getExcelData(filePath, sheetName)
+    testData = get_excel_data(filePath, sheetName)
     print(testData)
     # 将字符串转为字典
     # str2dict = eval(testData[0]['requestData'])
--- testCase/saveData.py
+++ testCase/saveData.py
@@ -1,27 +1,37 @@
+from ddt import ddt, data, unpack
+import sys
 import unittest
 import requests
-from basicMethod.useExcel import getExcelData
+from basicMethod.useExcel import get_excel_data
 from basicMethod.useMysql import delData
 
+rootDir = sys.path[1]
+filePath = rootDir + r"\testData\testData.xlsx"
+sheetName = "保存"
+test_data = get_excel_data(filePath, sheetName)
+
+
 # 保存数据
-class saveData(unittest.TestCase):
+@ddt
+class SaveData(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         delData()
-        filePath = r".\testData\testData.xlsx"
-        sheetName = "保存"
-        cls.test_data = getExcelData(filePath, sheetName)
+        # filePath = r".\testData\testData.xlsx"
+        # sheetName = "保存"
+        # cls.test_data = get_excel_data(filePath, sheetName)
         cls.header = {
             "User-Agent": "Mozilla / 5.0(Windows NT 6.1;",
             "Cookie": "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
         }
 
     def setUp(self) -> None:
+        pass
         # 执行一条测试用例，就将列表中的第一条数据pop出来
-        self.oneCase = self.test_data.pop(0)
-        self.requestUrl = self.oneCase["url"]
-        self.requestData = self.oneCase["requestData"]
-        self.responseData = self.oneCase["responseData"]
+        # self.oneCase = self.test_data.pop(0)
+        # self.requestUrl = self.oneCase["url"]
+        # self.requestData = self.oneCase["requestData"]
+        # self.responseData = self.oneCase["responseData"]
 
     def tearDown(self) -> None:
         pass
@@ -30,22 +40,29 @@ class saveData(unittest.TestCase):
         delData()
         # pass
 
-    def test001(self):
+    @data(*test_data)
+    @unpack
+    def test001(self, request_url, request_data, response_data):
         """姓名为空"""
-        self.resData = requests.post(self.requestUrl, header=self.header, json=self.requestData)
-        self.assertIn(self.responseData, self.resData.text)
+        self.resData = requests.post(request_url, header=self.header, json=request_data)
+        self.assertIn(response_data, self.resData.text)
 
-    def test002(self):
-        """电话为空"""
-        self.resData = requests.post(self.requestUrl, header=self.header, json=self.requestData)
-        self.assertIn(self.responseData, self.resData.text)
-
-    def test003(self):
-        """性别为空"""
-        self.resData = requests.post(self.requestUrl, header=self.header, json=self.requestData)
-        self.assertIn(self.responseData, self.resData.text)
-
-    def test004(self):
-        """正向用例"""
-        self.resData = requests.post(self.requestUrl, header=self.header, json=self.requestData)
-        self.assertIn(self.responseData, self.resData.text)
+    # def test001(self):
+    #     """姓名为空"""
+    #     self.resData = requests.post(self.requestUrl, header=self.header, json=self.requestData)
+    #     self.assertIn(self.responseData, self.resData.text)
+    #
+    # def test002(self):
+    #     """电话为空"""
+    #     self.resData = requests.post(self.requestUrl, header=self.header, json=self.requestData)
+    #     self.assertIn(self.responseData, self.resData.text)
+    #
+    # def test003(self):
+    #     """性别为空"""
+    #     self.resData = requests.post(self.requestUrl, header=self.header, json=self.requestData)
+    #     self.assertIn(self.responseData, self.resData.text)
+    #
+    # def test004(self):
+    #     """正向用例"""
+    #     self.resData = requests.post(self.requestUrl, header=self.header, json=self.requestData)
+    #     self.assertIn(self.responseData, self.resData.text)
