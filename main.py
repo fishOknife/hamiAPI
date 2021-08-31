1995ff5467eba71f8e2709d9c33317f5e6d5062f
well <42036951+fishorknife@users.noreply.github.com>
2021-08-31 15:02:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,4 +18,3 @@ if __name__ == "__main__":
     with open(fileName, "wb") as f:
         runner = HTMLTestRunner.HTMLTestRunner(stream=f, title="标题", description="测试结果", verbosity=2)
         runner.run(suite)
-#测试同步代码功能
